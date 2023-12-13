b69b2eeb4830d8c8dc033dac0376b7e6fc44e4cb
Alexey Dashko <alex.vipers@mail.ru>
2023-12-14 01:10:51 +0300

    test: separate package

--- test/index.mjs
+++ test/index.mjs
@@ -1,10 +1,10 @@
-import {Animelayer} from '../build/index.mjs';
+import {AnimeLayer} from 'animelayerjs';
 import fs from 'fs';
 
 (async function () {
-	const client = new Animelayer(fs.readFileSync('/home/alexv/Source/animelayerjs/cache/cookie').toString('ascii'));
+	const client = new AnimeLayer(fs.readFileSync('/home/alexv/Source/animelayerjs/cache/cookie').toString('ascii'));
 
-	const list = await client.search('hiki', '1920x1080');
+	const list = await client.search('Hikikomari Kyuuketsuki no Monmon', '1920x1080');
 
 	await Promise.all(list.map(async e => e.getMagnetUri(client)));
 
